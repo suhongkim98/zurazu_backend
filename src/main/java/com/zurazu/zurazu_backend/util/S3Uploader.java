58c77334814d2499dd2eb77ecae99898a9fc4914
suhongkim98 <kshz9815@naver.com>
2021-05-07 21:24:09 +0900

    임시파일 생성하지 않고 바로 S3에 업로드되도록 업로드 기능 수정

--- src/main/java/com/zurazu/zurazu_backend/util/S3Uploader.java
+++ src/main/java/com/zurazu/zurazu_backend/util/S3Uploader.java
@@ -2,6 +2,7 @@ package com.zurazu.zurazu_backend.util;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.PageAttributes.MediaType;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -28,53 +30,34 @@ import java.util.Optional;
  */
 
 
-@Slf4j
 @RequiredArgsConstructor
 @Component
 public class S3Uploader {
 
     private final AmazonS3Client amazonS3Client;
-    private final static String TEMP_FILE_PATH = "src/main/resources/";
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
 
-        return upload(uploadFile, dirName);
-    }
-
-    private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + uploadFile.getName();
+    public String upload(MultipartFile uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getOriginalFilename();
         String uploadImageUrl = putS3(uploadFile, fileName);
-        removeNewFile(uploadFile);
         return uploadImageUrl;
     }
 
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+    private String putS3(MultipartFile uploadFile, String fileName) {
+    	ObjectMetadata metadata = new ObjectMetadata();
+    	metadata.setContentType(uploadFile.getContentType());
+    	metadata.setContentLength(uploadFile.getSize());
+    	try {
+    	       amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile.getInputStream(), metadata).withCannedAcl(CannedAccessControlList.PublicRead));
+   		
+    	}catch (IOException e) {
+			// TODO: handle exception
+    		e.printStackTrace();
+		}
+         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
-    private void removeNewFile(File targetFile) {
-        if (targetFile.delete()) {
-            log.info("파일이 삭제되었습니다.");
-        } else {
-            log.info("파일이 삭제되지 못했습니다.");
-        }
-    }
-
-    private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(TEMP_FILE_PATH + file.getOriginalFilename());
-        if(convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-
-        return Optional.empty();
-    }
 }
