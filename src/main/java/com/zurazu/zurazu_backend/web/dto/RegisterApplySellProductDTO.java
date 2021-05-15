79ca69ac08157de64bec4ff199a876a105395429
suhongkim98 <kshz9815@naver.com>
2021-05-16 02:32:52 +0900

    size 설정 방식 변경

--- src/main/java/com/zurazu/zurazu_backend/web/dto/RegisterApplySellProductDTO.java
+++ src/main/java/com/zurazu/zurazu_backend/web/dto/RegisterApplySellProductDTO.java
@@ -4,6 +4,7 @@ import com.zurazu.zurazu_backend.core.GenderType;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -20,7 +21,7 @@ public class RegisterApplySellProductDTO {
     @NotEmpty(message = "옷 사이즈 입력이 필요합니다.")
     private String clothingSize;
     @NotNull(message = "옷 상태 입력이 필요합니다. 0~4")
-    @Size(max = 4)
+    @Max(4)
     private Integer clothingStatus;
     private GenderType gender = GenderType.MALE;
     private String comments;
