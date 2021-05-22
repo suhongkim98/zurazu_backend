3ed536a9d35f098652bb673c2bbc7ae99363fce3
suhongkim98 <kshz9815@naver.com>
2021-05-22 16:36:15 +0900

    어드민도 멤버인터셉터 통과하도록

--- src/main/java/com/zurazu/zurazu_backend/interceptor/MemberAuthInterceptor.java
+++ src/main/java/com/zurazu/zurazu_backend/interceptor/MemberAuthInterceptor.java
@@ -28,7 +28,7 @@ public class MemberAuthInterceptor implements HandlerInterceptor
         if(token.isPresent()) {
             //토큰이 존재한다면 해당 string을 객체로 변환해서 검증한다
             JwtAuthToken jwtAuthToken = jwtAuthTokenProvider.convertAuthToken(token.get());
-            if(jwtAuthToken.validate() && jwtAuthToken.getData().get("role").equals(Role.USER.getCode())) {
+            if(jwtAuthToken.validate()) {
                 return true;
             }
             throw new CustomJwtRuntimeException();
