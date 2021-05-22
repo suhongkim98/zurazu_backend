3907ecbd916fbd663b268e708fbe42382f18ce05
suhongkim98 <kshz9815@naver.com>
2021-05-22 21:40:38 +0900

    request method가 OPTIONS일 경우 CORS preflight 이슈 해결

--- src/main/java/com/zurazu/zurazu_backend/interceptor/AdminAuthInterceptor.java
+++ src/main/java/com/zurazu/zurazu_backend/interceptor/AdminAuthInterceptor.java
@@ -24,6 +24,10 @@ public class AdminAuthInterceptor implements HandlerInterceptor
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+    	if(request.getMethod().equals("OPTIONS")) {
+    		//request method가 OPTIONS일 경우 JWT를 체크하지 않는 방향으로
+    		return true;
+    	}
         Optional<String> token = jwtAuthTokenProvider.resolveToken(request);
         if(token.isPresent()) {
             //토큰이 존재한다면 해당 string을 객체로 변환해서 검증한다
